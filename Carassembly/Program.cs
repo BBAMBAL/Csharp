896064f4a84ea4b57135b713420326f7af416755
BBAMBAL <bhagyashribambal6@gmmail.com>
2021-12-29 16:59:14 +0530

    added

---
+++ Carassembly/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carassembly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            Console.WriteLine(c.SuccessRate(10));
+            Console.WriteLine(c.ProductionRatePerHour(6));
+            Console.WriteLine(c.WorkingItemsPerMinute(6));
+            Console.ReadLine();
+        }
+    }
+}
+
